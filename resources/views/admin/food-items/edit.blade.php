8df69a816f77b7da9d1b076ae3e03959263b1f45
Gerard David <gdavid32@hotmail.com>
2020-05-28 00:46:29 -0300

    Added controllers for admin and food-items routes

--- resources/views/admin/food-items/edit.blade.php
+++ resources/views/admin/food-items/edit.blade.php
@@ -9,14 +9,14 @@
       <div class="row">
           <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
               <div class="page-header">
-                  <h2 class="pageheader-title">Edit Food Category</h2>
+                  <h2 class="pageheader-title">Edit Food Items</h2>
                   <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                   <div class="page-breadcrumb">
                       <nav aria-label="breadcrumb">
                           <ol class="breadcrumb">
                               <li class="breadcrumb-item"><a href="/admin" class="breadcrumb-link">Dashboard</a></li>
-                              <li class="breadcrumb-item"><a href="/admin/food-categories" class="breadcrumb-link">All Food Catergories</a></li>
-                              <li class="breadcrumb-item active" aria-current="page">Edit Food Categories</li>
+                              <li class="breadcrumb-item"><a href="/admin/food-items" class="breadcrumb-link">All Food Items</a></li>
+                              <li class="breadcrumb-item active" aria-current="page">Edit Food Items</li>
                           </ol>
                       </nav>
                   </div>
@@ -32,16 +32,20 @@
               <!-- ============================================================== -->
               <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
                   <div class="card">
-                      <h5 class="card-header">Edit a new category</h5>
+                      <h5 class="card-header">Edit new item</h5>
                       <div class="card-body">
                           <form action="#" id="basicform" data-parsley-validate="">
                               <div class="form-group">
-                                  <label for="input-category">Category Name</label>
-                                  <input id="input-category" type="text" name="category" data-parsley-trigger="change" required="" placeholder="Enter category name" autocomplete="off" class="form-control">
+                                  <label for="input-item">Item name</label>
+                                  <input id="input-item" type="text" name="title" data-parsley-trigger="change" required="" placeholder="Enter item name" autocomplete="off" class="form-control">
                               </div>
                               <div class="form-group">
-                                  <label for="input-category-imageUrl">Category Image URL</label>
-                                  <input id="input-category-imageUrl" type="text" name="image_url" data-parsley-trigger="change" required="" placeholder="http://www.billys.com/burgers.jpg" autocomplete="off" class="form-control">
+                                  <label for="input-item">Item price</label>
+                                  <input id="input-item" type="text" name="price" data-parsley-trigger="change" required="" placeholder="Enter item price" autocomplete="off" class="form-control">
+                              </div>
+                              <div class="form-group">
+                                  <label for="input-item-imageUrl">Item Image URL</label>
+                                  <input id="input-item-imageUrl" type="text" name="image_url" data-parsley-trigger="change" required="" placeholder="http://www.billys.com/burgers.jpg" autocomplete="off" class="form-control">
                               </div>
                               <div class="row">
                                   <div class="col-sm-6 pb-2 pb-sm-4 pb-lg-0 pr-0">
