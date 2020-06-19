4ba219725ede036da2b7e95d8b84c53b786acf89
Gerard David <gdavid32@hotmail.com>
2020-06-19 01:59:37 -0300

    deleted extra migration and styled register.blade.php for auth

--- app/User.php
+++ app/User.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\CanResetPassword;
-use Illuminate\Auth\Passwords\CanResetPassword;
 
 class User extends Authenticatable
 {
